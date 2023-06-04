75729296def8840eb4ea527fec25a6a9b2235cd9
Hung Nguyen <hungnq1989@gmail.com>
2023-06-04 20:09:30 +0300

    Solution for part 2

--- 2022/go/day02.go
+++ 2022/go/day02.go
@@ -11,44 +11,59 @@ type Key struct {
 	you, opponent string
 }
 
-func calcScore(opponent, you string) int {
+func calcScorePart1(opponent, you string) int {
 	scores := map[Key]int{
-		{"A", "X"}: 3,
-		{"A", "Y"}: 6,
-		{"A", "Z"}: 0,
-		{"B", "X"}: 0,
-		{"B", "Y"}: 3,
-		{"B", "Z"}: 6,
-		{"C", "X"}: 6,
-		{"C", "Y"}: 0,
-		{"C", "Z"}: 3,
+		{"A", "X"}: 3 + 1,
+		{"A", "Y"}: 6 + 2,
+		{"A", "Z"}: 0 + 3,
+		{"B", "X"}: 0 + 1,
+		{"B", "Y"}: 3 + 2,
+		{"B", "Z"}: 6 + 3,
+		{"C", "X"}: 6 + 1,
+		{"C", "Y"}: 0 + 2,
+		{"C", "Z"}: 3 + 3,
 	}
 
-	Shapes := map[string]int{
-		"X": 1, // Rock
-		"Y": 2, // Paper
-		"Z": 3, // Scissors
+	return scores[Key{opponent, you}]
+}
+
+func scaleScorePart2(opponent, you string) int {
+	// X = lose, Y = draw, Z = win
+	// A = Rock, B = Paper, C = Scissors
+	scores := map[Key]int{
+		{"A", "X"}: 0 + 3,
+		{"A", "Y"}: 3 + 1,
+		{"A", "Z"}: 6 + 2,
+		{"B", "X"}: 0 + 1,
+		{"B", "Y"}: 3 + 2,
+		{"B", "Z"}: 6 + 3,
+		{"C", "X"}: 0 + 2,
+		{"C", "Y"}: 3 + 3,
+		{"C", "Z"}: 6 + 1,
 	}
 
-	return scores[Key{opponent, you}] + Shapes[you]
+	return scores[Key{opponent, you}]
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	score := 0
+	score1 := 0
+	score2 := 0
 	for scanner.Scan() {
 		line := strings.TrimRight(scanner.Text(), "\n")
 		letters := strings.Split(line, " ")
 		opponent := letters[0]
 		you := letters[1]
 
-		score += calcScore(opponent, you)
+		score1 += calcScorePart1(opponent, you)
+		score2 += scaleScorePart2(opponent, you)
 	}
 
 	if scanner.Err() != nil {
 		fmt.Println("Error:", scanner.Err())
 	}
 
-	fmt.Println("Score:", score)
+	fmt.Println("Score 1:", score1)
+	fmt.Println("Score 2:", score2)
 }
