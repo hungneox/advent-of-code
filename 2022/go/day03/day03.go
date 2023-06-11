b864f2d14cd280c4b02bf4bdbcea13df914c71e0
Hung Nguyen <hungnq1989@gmail.com>
2023-06-11 19:04:34 +0300

    Add part 2 for day 03

--- 2022/go/day03/day03.go
+++ 2022/go/day03/day03.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func commonLetter(s1, s2 string) rune {
+func commonLetterOfTwo(s1, s2 string) rune {
 	var common rune
 out:
 	for i := 0; i < len(s1); i++ {
@@ -16,6 +16,23 @@ out:
 				common = rune(s1[i])
 				break out
 			}
+		}
+	}
+	return common
+}
+
+func commonLetterOfThree(s1, s2, s3 string) rune {
+	var common rune
+out:
+	for i := 0; i < len(s1); i++ {
+		for j := 0; j < len(s2); j++ {
+			for k := 0; k < len(s3); k++ {
+				if s1[i] == s2[j] && s2[j] == s3[k] {
+					common = rune(s1[i])
+					break out
+				}
+			}
+
 		}
 	}
 	return common
@@ -32,7 +49,7 @@ func alphabetToNumber(s rune) int {
 	return int(s) - 96
 }
 
-func main() {
+func partOne() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	sum := 0
@@ -42,7 +59,7 @@ func main() {
 		part1, part2 := line[:len(line)/2], line[len(line)/2:]
 
 		// Find common letter
-		common := commonLetter(part1, part2)
+		common := commonLetterOfTwo(part1, part2)
 
 		// Increase sum by the priroty of common letter
 		sum += alphabetToNumber(common)
@@ -53,4 +70,38 @@ func main() {
 	}
 
 	fmt.Println("Sum:", sum)
+}
+
+func partTwo() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	sum := 0
+	lineCount := 0
+	lines := make([]string, 3)
+	for scanner.Scan() {
+
+		line := strings.TrimRight(scanner.Text(), "\n")
+		lines[lineCount] = line
+		lineCount++
+
+		if lineCount == 3 {
+			// Find common letter
+			common := commonLetterOfThree(lines[0], lines[1], lines[2])
+			// Increase sum by the priroty of common letter
+			sum += alphabetToNumber(common)
+
+			lineCount = 0
+		}
+	}
+
+	if scanner.Err() != nil {
+		fmt.Println("Error:", scanner.Err())
+	}
+
+	fmt.Println("Sum:", sum)
+}
+
+func main() {
+	// partOne()
+	partTwo()
 }
