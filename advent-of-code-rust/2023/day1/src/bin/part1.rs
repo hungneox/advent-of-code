4abd0e44e457245a4dd2738752a8e2e4d7b61633
Hung Nguyen <hungnq1989@gmail.com>
2023-12-16 19:06:31 +0200

    Change to fp approach

--- advent-of-code-rust/2023/day1/src/bin/part1.rs
+++ advent-of-code-rust/2023/day1/src/bin/part1.rs
@@ -2,43 +2,19 @@ use std::fs::read_to_string;
 
 fn main() {
     println!("Advent of code 2023 - day 1 part 1");
-    let inputs = read_lines("./inputs/input.txt");
+    let inputs = read_lines("./inputs/sample.txt");
 
-    let mut two_digits_numbers = Vec::new();
+    let result = inputs
+        .iter()
+        .map(|line| {
+            let first_digit = line.chars().find(|c| c.is_ascii_digit()).unwrap();
+            let second_digit = line.chars().rev().find(|c| c.is_ascii_digit()).unwrap();
 
-    for input in inputs {
-        // println!("{}", input);
-        let numbers = find_numbers(&input);
-        two_digits_numbers.push(numbers)
-    }
+            return first_digit.to_digit(10).unwrap() * 10 + second_digit.to_digit(10).unwrap();
+        })
+        .sum::<u32>();
 
-    let total: u32 = two_digits_numbers.iter().sum();
-    // println!("Vector<u32>{:?}", two_digits_numbers);
-    println!("Result {}", total)
-}
-
-fn find_numbers(input: &str) -> u32 {
-    let mut numbers = Vec::new();
-
-    for char in input.chars() {
-        if char.is_ascii_digit() {
-            numbers.push(char);
-        }
-    }
-
-    let nlen = numbers.len();
-
-    let mut result: String = "0".to_string();
-
-    if numbers.len() >= 2 {
-        result = format!("{}{}", numbers[0], numbers[nlen - 1]);
-    }
-
-    if numbers.len() == 1 {
-        result = format!("{}{}", numbers[0], numbers[0]);
-    }
-
-    return result.parse::<u32>().unwrap();
+    println!("Result {}", result)
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
