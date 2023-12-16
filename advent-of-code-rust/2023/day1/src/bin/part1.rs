241833c1a007dea8e6790a7e70196917a4b8b6d0
Hung Nguyen <hungnq1989@gmail.com>
2023-12-16 19:33:57 +0200

    Use unwrap_or('0') to make sure it always work

--- advent-of-code-rust/2023/day1/src/bin/part1.rs
+++ advent-of-code-rust/2023/day1/src/bin/part1.rs
@@ -2,13 +2,17 @@ use std::fs::read_to_string;
 
 fn main() {
     println!("Advent of code 2023 - day 1 part 1");
-    let inputs = read_lines("./inputs/sample.txt");
+    let inputs: Vec<String> = read_lines("./inputs/sample.txt");
 
     let result = inputs
         .iter()
         .map(|line| {
-            let first_digit = line.chars().find(|c| c.is_ascii_digit()).unwrap();
-            let second_digit = line.chars().rev().find(|c| c.is_ascii_digit()).unwrap();
+            let first_digit: char = line.chars().find(|c| c.is_ascii_digit()).unwrap_or('0');
+            let second_digit: char = line
+                .chars()
+                .rev()
+                .find(|c| c.is_ascii_digit())
+                .unwrap_or('0');
 
             return first_digit.to_digit(10).unwrap() * 10 + second_digit.to_digit(10).unwrap();
         })
